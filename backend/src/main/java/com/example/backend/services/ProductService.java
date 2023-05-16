50ca73153618a8fcd96bf1cdbedf309cce560acf
JRejowski <janrejowski39@gmail.com>
2023-05-16 19:32:17 +0200

    Added services

---
+++ backend/src/main/java/com/example/backend/services/ProductService.java
@@ -0,0 +1,34 @@
+package com.example.backend.services;
+
+import com.example.backend.models.Product;
+import com.example.backend.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public ProductService(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(Long productId) {
+        return productRepository.findById(productId).orElse(null);
+    }
+
+    public List<Product> getProductsByCategory(String category) {
+        return productRepository.findByCategory(category);
+    }
+
+
+}
+
---
+++ backend/src/main/java/com/example/backend/services/UserService.java
@@ -0,0 +1,30 @@
+package com.example.backend.services;
+
+import com.example.backend.models.User;
+import com.example.backend.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId).orElse(null);
+    }
+
+
+}

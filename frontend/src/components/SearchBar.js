a2660496351be61210eb4ec1dccca72f9afff954
JRejowski <janrejowski39@gmail.com>
2023-06-04 22:01:51 +0200

    Added products search

--- frontend/src/components/SearchBar.js
+++ frontend/src/components/SearchBar.js
@@ -1,9 +1,26 @@
+import React, { useState } from "react";
 
-function SearchBar(){
+function SearchBar({ onSearch }) {
+    const [query, setQuery] = useState("");
+
+    const handleSearch = () => {
+        onSearch(query);
+    };
+
+    const handleKeyDown = (e) => {
+        if (e.key === "Enter") {
+            handleSearch();
+        }
+    };
 
     return (
         <div className="search-bar">
-            <input placeholder="search supplements"/>
+            <input
+                placeholder="search supplements"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+                onKeyDown={handleKeyDown}
+            />
         </div>
     );
 }

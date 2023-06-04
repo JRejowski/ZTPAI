1ccc9c6f5f8b4a2d8472c2e0bd49398d0d1d0b63
JRejowski <janrejowski39@gmail.com>
2023-06-04 22:01:15 +0200

    Added routes for products

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -7,6 +7,7 @@ import Register from './Register';
 import Profile  from './Profile';
 import Settings from './Settings';
 import Rankings from "./Rankings";
+import Product from "./Product";
 
 function App() {
     return (
@@ -15,6 +16,7 @@ function App() {
                 <Route exact path="/" component={Login} />
                 <Route exact path="/login" component={Login} />
                 <Route exact path="/home" component={Home} />
+                <Route exact path="/products/:productId" component={Product} />
                 <Route exact path="/register" component={Register} />
                 <Route exact path="/profile" component={Profile} />
                 <Route exact path="/settings" component={Settings} />

05ce8950bcfb7aabbe9ed7b806a5016903b405ea
JRejowski <janrejowski39@gmail.com>
2023-06-06 00:04:00 +0200

    Updated react-router-dom to v6

--- frontend/src/js/App.js
+++ frontend/src/js/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import { Switch } from 'react-router-dom';
 import Home from './Home';
 import Login from './Login';
 import Register from './Register';
